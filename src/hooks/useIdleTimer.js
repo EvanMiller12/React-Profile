20974af79f909a53dc31d0a541c4ec3cafe4b915
evan miller <evanchancemiller@gmail.com>
2024-03-08 09:41:45 -0500

    chore: cleanup idleTimer hook

--- src/hooks/useIdleTimer.js
+++ src/hooks/useIdleTimer.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useIdleTimer } from "react-idle-timer";
 
 function useIdle({ onIdle, idleTime }) {
@@ -7,21 +7,6 @@ function useIdle({ onIdle, idleTime }) {
   //handles what happens when the user is idle
   const handleOnIdle = event => {
     setIsIdle(true); //set the state to true
-    const currentTime = new Date();
-    const formattedCurrentTime = currentTime.toLocaleString("en-US", {
-      weekday: "short",
-      year: "numeric",
-      month: "short",
-      day: "numeric",
-      hour: "numeric",
-      minute: "numeric",
-      second: "numeric",
-      timeZoneName: "short"
-    });
-
-    // console.log("user is idle", event); //log the user is idle followed by the event
-    // console.log("Last Active time", getLastActiveTime()); // you the log the time the user was last active
-    // console.log("Current time", formattedCurrentTime); //gets the curent time maybe to see what
 
     onIdle(); //then call onIdle function
   };
