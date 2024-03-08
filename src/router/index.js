c8253f5cfe19566f6ca33d2112bb2070798a5700
evan miller <evanchancemiller@gmail.com>
2024-03-08 09:39:13 -0500

    chore: move router to router dir

---
+++ src/router/index.js
@@ -0,0 +1,73 @@
+import { createBrowserRouter, redirect } from "react-router-dom";
+import { currentUser } from "../auth";
+
+// components
+import { Layout } from "../components/Layout";
+import { LoginPage } from "../components/Login";
+import { ProfileCreateEditPage } from "../components/Profile/ProfileCreateEdit";
+import { ProfilePage } from "../components/Profile";
+
+// route actions
+import loginAction from "./actions/loginAction";
+import createEditAction from "./actions/createEditAction";
+
+// route loaders
+import loginLoader from "./loaders/loginLoader";
+import protectedRouteLoader from "./loaders/protectedRouteLoader";
+
+const router = createBrowserRouter([
+  {
+    id: "root",
+    path: "/",
+    loader() {
+      // Our root route always provides the users email, if logged in
+      // used to manage user in local storaage
+      return { user: currentUser?.email };
+    },
+    Component: Layout,
+    children: [
+      {
+        path: "login",
+        action: loginAction,
+        loader: loginLoader,
+        Component: LoginPage
+      },
+      {
+        path: "create-edit-profile",
+        action: createEditAction,
+        loader: protectedRouteLoader,
+        Component: ProfileCreateEditPage
+      },
+      {
+        path: "profile",
+        loader: protectedRouteLoader,
+        Component: ProfilePage
+      }
+    ]
+  },
+  {
+    path: "/logout",
+    async action() {
+      // We signout in a "resource route" that we can hit from a fetcher.Form
+      await currentUser.signout();
+      return redirect("/");
+    }
+  },
+  {
+    path: "/session-timeout",
+    async loader() {
+      await currentUser.signout();
+      return redirect("/login");
+    }
+  },
+  {
+    path: "/delete-profile",
+    async action() {
+      // We signout in a "resource route" that we can hit from a fetcher.Form
+      await currentUser.delete(currentUser?.email);
+      return redirect("/login");
+    }
+  }
+]);
+
+export default router;
